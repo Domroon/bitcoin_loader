5d69f64e2b511e0f59a0905f628420653127f6ef
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-24 15:40:28 +0200

    print all currencies

--- bitcoin_loader.py
+++ bitcoin_loader.py
@@ -9,9 +9,28 @@ def download_json(url):
     return data
 
 def main():
+    json_file = download_json("http://api.coindesk.com/v1/bpi/currentprice.json")
+
+    eur_code = str(json_file["bpi"]["EUR"]["code"]) + " "
+    gbp_code = str(json_file["bpi"]["GBP"]["code"]) + " "
+    usd_code = str(json_file["bpi"]["USD"]["code"]) + " "
+
+    eur_description = "(" + str(json_file["bpi"]["EUR"]["description"]) + ")"
+    gbp_description = "(" + str(json_file["bpi"]["GBP"]["description"]) + ")"
+    usd_description = "(" + str(json_file["bpi"]["USD"]["description"]) + ")"
+
+
     while True:
         json_file = download_json("http://api.coindesk.com/v1/bpi/currentprice.json")
-        print(json_file["bpi"]["EUR"]["rate_float"])
+        print("1 bitcoin equals: ")
+
+        eur_price = str(json_file["bpi"]["EUR"]["rate_float"]) + " € "
+        gbp_price = str(json_file["bpi"]["GBP"]["rate_float"]) + " £ "
+        usd_price = str(json_file["bpi"]["USD"]["rate_float"]) + " $ "
+
+        print(eur_code + eur_price  + eur_description)
+        print(gbp_code + gbp_price  + gbp_description)
+        print(usd_code + usd_price  + usd_description + "\n")
         time.sleep(60)
 
 if __name__ == '__main__':
